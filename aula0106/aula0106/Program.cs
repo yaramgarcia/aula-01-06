c5140bf69a8013dfa8b409eabb47c88c68c0fd94
Yara Garcia <yara.garcia@etec.sp.gov.br>
2022-06-01 22:28:24 -0300

    uso do select

--- aula0106/aula0106/Program.cs
+++ aula0106/aula0106/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace aula0106
 {
@@ -10,6 +12,28 @@ namespace aula0106
     {
         static void Main(string[] args)
         {
+            MySqlConnection conexao = new MySqlConnection ("server=localhost;database=des;uid=root;password=");
+            try
+            {
+                conexao.Open();
+            }
+            catch(MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Ocorreu um erro, fale com o Pedro!");
+                Environment.Exit(0);
+            }
+            string sql = "select * from login";
+            MySqlCommand cmd = new MySqlCommand(sql,conexao);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            if (rdr.HasRows)
+            {
+                Console.WriteLine("Lista de login");
+
+            } else
+            {
+                Console.WriteLine("Sem dados para exibição");
+            }
         }
     }
 }
